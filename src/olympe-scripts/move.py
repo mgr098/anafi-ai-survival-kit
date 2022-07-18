3f6c19bee5c24538f8239ddd5de42726b91ee5cb
mgr098 <mgr098@uit.no>
2022-07-18 16:21:48 +0200

    ✨ add automatic takeoff

--- src/olympe-scripts/move.py
+++ src/olympe-scripts/move.py
@@ -3,6 +3,7 @@ import os
 
 from olympe.messages.ardrone3.Piloting import TakeOff, moveBy, Landing
 from olympe.messages.ardrone3.PilotingState import FlyingStateChanged
+from olympe.messages.ardrone3.GPSSettingsState import GPSFixStateChanged
 
 DRONE_IP = os.environ.get("DRONE_IP", "192.168.42.1")
 
@@ -10,11 +11,26 @@ def test_takeoff_docker():
     drone = olympe.Drone(DRONE_IP)
     drone.connect()
     
-    assert drone(TakeOff()).wait().success()
+    # Automatic takeoff
+    drone(
+        FlyingStateChanged(state="hovering", _policy="check")
+        | FlyingStateChanged(state="flying", _policy="check")
+        | (
+            GPSFixStateChanged(fixed=1, _timeout=5, _policy="check_wait")
+            >> (
+                TakeOff(_no_expect=True)
+                & FlyingStateChanged(
+                    state="hovering", _timeout=5, _policy="check_wait"
+                )
+            )
+        )
+    ).wait()
+
     assert drone(
         moveBy(10, 0, 0, 0) 
         >> FlyingStateChanged(state="hovering", _timeout=5)
-    ).wait().success()    
+    ).wait().success()
+        
     assert drone(Landing()).wait().success()
 
     drone.disconnect()
