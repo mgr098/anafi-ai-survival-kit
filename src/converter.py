abbedb095df4b1adcd68990553f7886f5a38660f
mgr098 <mgr098@uit.no>
2022-07-21 12:37:07 +0200

    ✨ update converter: now works

--- src/converter.py
+++ src/converter.py
@@ -3,17 +3,16 @@ import json
 import logging
 import argparse
 
-LATEST_VERSION = "120"
-TAKEOFF = 22
-WAYPOINT = 16
-DEFAULT_FILE_NAME = "1.mavlink"
+from config import LATEST_VERSION, DEFAULT_FILE_NAME, TAKEOFF, WAYPOINT
 
-logger=logging.getLogger()
+logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
+
 
 # Parses input arguments
 def parse_args():
-    parser = argparse.ArgumentParser(description="Convert QGC .plan to .mavlink format")
+    parser = argparse.ArgumentParser(
+        description="Convert QGC .plan to .mavlink format")
 
     parser.add_argument(
         "filepath", type=str, help="Usage: python3 main.py </path/to/file/>")
@@ -32,9 +31,11 @@ This class manages the .plan conversion by
 loading the .plan, initializing the mav object
 and writing the formated mav object to a new file
 """
+
+
 class Converter():
     def __init__(self, filepath, out, takeoff, version):
-        self.filepath = filepath 
+        self.filepath = filepath
         self.out = out
         self.takeoff = takeoff
         self.version = version
@@ -49,7 +50,7 @@ class Converter():
 
     def verify_format(self):
         """Verifies plan format"""
-        #TODO: check if its a .plan, and that its not empty
+        # TODO: check if its a .plan, and that its not empty
         try:
             with open(self.filepath) as f:
                 self.plan = json.load(f)
@@ -67,40 +68,43 @@ class Converter():
                     f.write(str(line))
                 f.close()
         except:
-            logging.exception("Unexpected error, could not append MAVlink object to file")
+            logging.exception(
+                "Unexpected error, could not append MAVlink object to file")
+
 
 """
 This class converts a JSON .plan file to a 
 formated mavlink file
 """
+
+
 class Mav():
     def __init__(self, plan, version, takeoff):
         self.plan = plan
         self.header = "QGC WPL {}".format(version)
         self.takeoff = takeoff
-        
+
         self.mission_items = self.convert()
         self.file = self.format_items()
-    
+
     def convert(self):
-        """Convert plan according to MAVlink"""
-        mission_items = []
+        """Convert plan to mavlink plaintext file format"""
 
-        for i, item in enumerate(self.plan["mission"]["items"]):
-            if item["type"] == "SimpleItem":
-                params = ["Nan" if i is None else i for i in item["params"]]
+        mav_items = []
+        plan_items = self.plan["mission"]["items"]
 
-                mission_item = [
-                    i, 0, item["frame"], 
-                    item["command"], *params, 1 if item ["autoContinue"] else 0
-                ]
+        for i, item in enumerate(plan_items):
+            frame = item["frame"]
+            command = item["command"]
 
-                mission_items.append(mission_item)
-            else:
-                logging.exception("Cannot convert type: complexItem")
-                sys.exit(0)
-        # print(mission_items)
-        return self.set_current_wp(mission_items)
+            auto_continue = 1 if item["autoContinue"] else 0
+            parameters = ["Nan" if i is None else "{:.6f}".format(
+                i) for i in item["params"]]
+
+            mav_item = [i, 0, frame, command, *parameters, auto_continue]
+            mav_items.append(mav_item)
+
+        return self.set_current_wp(mav_items)
 
     def format_items(self):
         mav_file = []
@@ -109,7 +113,7 @@ class Mav():
         # Not sure if this is needed.
         # However, parrot drones need to takeoff
         # before other drone functions are activated
-        if self.takeoff: 
+        if self.takeoff:
             self.takeoff = [0, 1, 3, TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 0]
             mav_file.append("\n")
             mav_file.append(self.insert_tabs(self.takeoff))
@@ -119,20 +123,27 @@ class Mav():
             mav_file.append(self.insert_tabs(line))
 
         return mav_file
-    
+
     def set_current_wp(self, mission_items):
         """Finds and sets current waypoint flag"""
-
+        flag = 0
         for item in mission_items:
-            if item[3] in (WAYPOINT, TAKEOFF):
+            if item[3] is TAKEOFF and flag == 0:
                 item[1] = 1
-                break
-        
+                flag = 1
+
+            if item[3] is WAYPOINT:
+                return mission_items
+
+        logging.warning(
+            "No WAYPOINT detected! GroundSDK Flightplans require WAYPOINT to run flightplans")
+
         return mission_items
 
     def insert_tabs(self, target):
         """Insert tab between every item in target"""
-        return "\t".join(str(t) for t in target)
+
+        return"\t\t".join(str(t) for t in target)
 
 
 if __name__ == "__main__":
